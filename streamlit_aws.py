a824a2969d13600fcef71f3e774b48975ff727b1
get-conected <conected@vit.edu.in>
2023-01-02 15:05:27 +0530

    Added 2 buckets

--- streamlit_aws.py
+++ streamlit_aws.py
@@ -3,24 +3,35 @@ import boto3
 import os
 import json
 
+output_bucket='final-output-0123456'
+input_bucket='image-test-0123456'
 
-input_bucket='custom-labels-console-ap-northeast-1-c8d279ec4d'
-
-#Upload to streamlit
+# Upload to streamlit
 uploader= st.file_uploader("Choose a file")
 #Upload to s3
 if uploader is not None:
     s3 = boto3.client('s3')
     s3.upload_fileobj(uploader, input_bucket, uploader.name)
+    name=uploader.name
     st.write("File uploaded")
-    
+    #Change file name
+    # st.write(name)
+    base=os.path.splitext(name)[1]
+    # st.write(base)
+    file_name=name.replace(base, '.json')
+    #st.write(file_name)
     # Download from s3
     s3_r=boto3.resource("s3")
-    my_bucket=s3_r.Bucket(input_bucket)
+    # file_name="78666984.json"
+    my_bucket=s3_r.Bucket(output_bucket)
     for object in my_bucket.objects.all():
         #print(object)
-        if(object.key.endswith(".json")):
-            s3.download_file(input_bucket, object.key, object.key)
+        st.write(object.key)
+        if(object.key==file_name):
+            st.write("File found")
+            # response = client.refresh_cache(
+            #         FileShareARN='arn:aws:s3:::final-output-0123456')
+            s3.download_file(output_bucket, object.key, object.key)
             st.write("File downloaded")
             # Read json file
             with open(object.key) as f:
